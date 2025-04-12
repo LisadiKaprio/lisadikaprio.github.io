1422212ee07cc33c5efed5b28c06d9cdf229ac1f
LisadiKaprio <lisadikaprio@gmail.com>
2025-04-12 12:20:16 +0000

    Github Actions: Update image list

--- images.js
+++ images.js
@@ -1,7 +1,10 @@
 const drawings = [
-  'drawings/drawings/IMG_20250411-.jpg',
-  'drawings/drawings/NewCanvas1.png',
-  'drawings/drawings/evil-emo-neuro.jpg',
-  'drawings/drawings/image.png',
-  'drawings/drawings/neuro-things.png',
+  'drawings/IMG_20250411-.jpg',
+  'drawings/NewCanvas1.png',
+  'drawings/edgy-evil-01082024.png',
+  'drawings/evil-emo-neuro.jpg',
+  'drawings/image.png',
+  'drawings/neuro-pajama.png',
+  'drawings/neuro-queen.jpg',
+  'drawings/neuro-things.png',
 ];
